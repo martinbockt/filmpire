22b647af809a54df9ac966a51ebc17d596c1b52b
Martin Bock <mail.martinbock@gmail.com>
2022-06-17 15:40:48 +0200

    Add MUI Theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import App from './components/App'
 
+const theme = createTheme({})
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
-    <Router>
-        <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+        <Router>
+            <App />
+        </Router>
+    </ThemeProvider>
 )
