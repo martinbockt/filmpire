8eecbe6a6b99cfc0c4273648ac39a6e19d727c14
Martin Bock <mail.martinbock@gmail.com>
2022-06-17 17:12:43 +0200

    redux + api setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import {BrowserRouter as Router} from 'react-router-dom'
-import { createTheme, ThemeProvider } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material'
+import { Provider } from 'react-redux'
 
 import App from './components/App'
+import store from './app/store'
 
 const theme = createTheme({})
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
-    <ThemeProvider theme={theme}>
-        <Router>
-            <App />
-        </Router>
-    </ThemeProvider>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <App />
+            </Router>
+        </ThemeProvider>
+    </Provider>
 )
