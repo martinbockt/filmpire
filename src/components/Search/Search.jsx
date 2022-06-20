13e698a721b5e23e0039fddc6be8d1f0cf3626b3
Martin Bock <mail.martinbock@gmail.com>
2022-06-20 14:29:35 +0200

    adding search

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect} from 'react'
+import { TextField, InputAdornment, Box } from '@mui/material'
+import { Search as SearchIcon } from '@mui/icons-material'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
+import { useTheme } from '@mui/material/styles'
+
+import { searchMovie } from '../../features/currentGenreOrCategory'
+
+import useStyles from './styles'
+
+const Search = () => {
+
+    const theme = useTheme()
+    const classes = useStyles({theme})
+    const [query, setQuery] = useState('')
+    const dispatch = useDispatch()
+
+    const handleKeyPress = (event) => {
+        if(event.key === "Enter") {
+            dispatch(searchMovie(query))
+        }
+    }
+
+    return (
+        <Box sx={classes.searchContainer}>
+            <TextField 
+                onKeyPress={handleKeyPress}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                variant="standard"
+                InputProps={{
+                    sx: classes.input, 
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    )
+                    }}
+            />
+        </Box>
+    )
+}
+
+export default Search
---
+++ src/components/Search/styles.js
@@ -0,0 +1,23 @@
+import { styled } from '@mui/material/styles';
+
+
+const useStyles = ({theme}) => ({
+
+    searchContainer: {
+        [theme.breakpoints.down('sm')]: {
+            display: 'flex',
+            justifyContent: 'center',
+            width: '100%'
+        }
+    },
+    input: {
+        color: theme.palette.mode === 'light' && 'black',
+        filter: theme.palette.mode === 'light' && 'invert(1)',
+        [theme.breakpoints.down('sm')]: {
+            marginTop: '-10px',
+            marginBottom: '10px'
+        }
+    }
+})
+
+export default useStyles
